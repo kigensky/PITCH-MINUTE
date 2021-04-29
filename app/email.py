a3ef4578316a98cadd2fbd5622ff535c2034dd0c
victor kigen <vickigen@gmail.com>
2021-04-29 07:51:52 +0300

    update

--- app/email.py
+++ app/email.py
@@ -2,6 +2,7 @@ from flask_mail import Message
 from flask import render_template
 from . import mail
 
+#add mail
 def mail_message(subject,template,to,**kwargs):
     sender_email = 'kenyakigen64@gmail.com'
     email = Message(subject, sender=sender_email, recipients=[to])
